ad98a361437c37141dc75a1e4c7a18192110c7cb
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-23 13:05:49 -0500

    Solucion ejercicio

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,30 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
+  #include Devise::Controllers::Helpers
+  #helper_method :current_user
+
   def index
-    @expenses = Expense.order('date DESC')
-   	@expenses =  @expenses.concept(params[:concept]) if params[:concept].present?
-   	@expenses= @expenses.category_id(params[:category_id]) if params[:category_id].present?
+    user = current_user
+  	if params[:concept] or params[:category_id]
+  	  @expenses = Expense.search(user.id, params[:concept], params[:category_id].to_i).order("date DESC")
+  	else
+  	  @expenses = Expense.where("user_id = ? ", "#{user.id}").order("date DESC")
+  	end
   end
+
+
+  #def index
+  #  if current_user != nil
+  #    @expenses = Expense.order('date DESC')
+  #   	@expenses =  @expenses.concept(params[:concept]) if params[:concept].present?
+  #   	@expenses = @expenses.category_id(params[:category_id]) if params[:category_id].present?
+  #  end
+
+    #@expenses = current_user.@expenses
+    #@user = current_user.email
+    #@curren_user = current_user
+  #end
 end
 
 #@expenses = Expense.order("date DESC")
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,15 @@
 class Expense < ActiveRecord::Base
   belongs_to :category
+  belongs_to :user
 
-  scope :category_id, -> (category_id) { where category_id: category_id }
-  scope :concept, -> (concept) { where("concept like ?", "%#{concept}%")}
+  def self.search(user_id, concept, category_id)
+    if category_id != 0
+      where("user_id = ? AND concept like ? AND category_id = ?", "#{user_id}", "%#{concept}%", "#{category_id}")
+    else
+      where("user_id = ? AND concept like ? ", "#{user_id}", "%#{concept}%")
+    end
+  end
+
+  #scope :category_id, -> (category_id) { where category_id: category_id }
+  #scope :concept, -> (concept) { where("concept like ?", "%#{concept}%")}
 end
