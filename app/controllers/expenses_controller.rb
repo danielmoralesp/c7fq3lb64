61b668e2cbaefddd17610d24e1574d1c10b4b06b
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-23 08:22:53 -0500

    solucion sin downcase

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,28 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    my_where_params = params.permit(:concept, :category_id).select {|k,v| v.present?}
+    expenses = Expense.where(my_where_params)
+    @expenses = expenses.order("date DESC")
   end
 end
+
+#@expenses = Expense.order("date DESC")
+#product = Product.find(params[:product_id])
+#@product = Product.find(params[:id])
+#@expenses = Expense.find(params[:category_id])
+
+#expenses = Expense.where(params[:category_id])
+#@expenses = expenses.order("date DESC")
+
+=begin
+Solucion que funciona explicitamente
+my_where_params = params.permit(:concept, :category_id).select {|k,v| v.present?}
+expenses = Expense.where(my_where_params)
+@expenses = expenses.order("date DESC")
+
+if params[:concept, :category_id].present?
+  search = params[:concept, :category_id].downcase
+  expenses = Expense.where(search)
+  @expenses = expenses.order("date DESC")
+end
+=end
